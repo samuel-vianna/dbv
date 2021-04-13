93ef5dbe2ff9b6551539943f579280cc778b3bb1
samuel-vianna <samuel-vianna@hotmail.com>
2021-04-13 13:53:53 -0300

    v1

--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,13 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Cidade Canção</title>
+        {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
 
       <main className={styles.main}>
         <h1> Em Breve</h1>
+        <img src='/logo-clube.jpg' alt='logo clube' id={styles.logo} />
       </main>
 
       <footer className={styles.footer}>
